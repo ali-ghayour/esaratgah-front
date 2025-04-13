95d8cea279bf02b463e74bb855e8ef59c6e1b6a3
ali-ghayour <ali.ghayour.se@gmail.com>
2025-04-13 17:36:52 +0330

    fixed select user avatar at edit users

--- src/app/customProviders/FileManager/FileManagerModal.tsx
+++ src/app/customProviders/FileManager/FileManagerModal.tsx
@@ -4,19 +4,17 @@ import { DropZone } from "./components/DropZone";
 import { FilesList } from "./components/FilesList";
 import { FilesListPagination } from "./components/FilesListPagination";
 import { useFileManagerModal } from "./useFileManagerModal";
-import {
-  useQueryResponse,
-} from "./core/QueryResponseProvider";
+import { useQueryResponse } from "./core/QueryResponseProvider";
 
 export const FileManagerModal = () => {
-  const { showModal, handleClose } = useFileManagerModal();
+  const { showModal, handleClose, handleSelectFiles } = useFileManagerModal();
   const { refetch, isLoading } = useQueryResponse();
 
   useEffect(() => {
     if (showModal) {
       refetch(); // Manually trigger the query
     }
-  }, [showModal,refetch]);
+  }, [showModal, refetch]);
 
   return (
     <Modal
@@ -45,7 +43,9 @@ export const FileManagerModal = () => {
         <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button variant="primary">Select Files</Button>
+        <Button variant="primary" onClick={handleSelectFiles}>
+          Select Files
+        </Button>
       </Modal.Footer>
     </Modal>
   );
